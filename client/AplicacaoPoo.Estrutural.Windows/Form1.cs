9e50f9fbb49c7a6093f2159e539baa441cdd8808
Alessandra Lopes <alessandra.lopesiqp@gmail.com>
2022-09-15 22:00:12 -0300

    Formulário Principal

---
+++ client/AplicacaoPoo.Estrutural.Windows/Form1.cs
@@ -0,0 +1,10 @@
+namespace AplicacaoPoo.Estrutural.Windows
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+    }
+}
